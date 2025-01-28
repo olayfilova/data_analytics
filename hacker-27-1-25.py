e91da0e028c41ad393ce83ff9d31b2dadbcc6a7e
olafilova <ofilova@gmail.com>
2025-01-28 09:42:48 +0000

    numpy

--- hacker-27-1-25.py
+++ hacker-27-1-25.py
@@ -1,22 +1,8 @@
 import numpy as np
-from itertools import product
 
-X, Y = map(int, input().split())
-arr = np.array([list(map(int, input().split())) for _ in range(X)])
-sum_result = np.sum(arr, axis=0)
-prod_result = np.prod(sum_result)
-
-print(prod_result)
-##
-list1 = [1, 2]
-list2 = ['a', 'b']
-
-result = product(list1, list2)
-print(list(result))
-##
 X, Y = map(int, input().split())
 
 array = np.array([list(map(int, input().split())) for _ in range(X)])
-sum_result = np.sum(array, axis=0)
-product_result = np.prod(sum_result)
-print(product_result)
+row_min = np.min(array, axis=1)
+result = np.max(row_min)
+print(result)
