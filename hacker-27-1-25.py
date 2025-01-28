47b64ed027c85592fa02e3a8f074c2f9471d577f
olafilova <ofilova@gmail.com>
2025-01-28 09:43:50 +0000

    numpy

--- hacker-27-1-25.py
+++ hacker-27-1-25.py
@@ -2,7 +2,12 @@ import numpy as np
 
 X, Y = map(int, input().split())
 
-array = np.array([list(map(int, input().split())) for _ in range(X)])
-row_min = np.min(array, axis=1)
-result = np.max(row_min)
-print(result)
+array = np.array([list(map(int, input().split())) for i in range(X)])
+
+mean_res = np.mean(array, axis=1)
+var_res = np.var(array, axis=0)
+std_res = round(np.std(array), 1)
+
+print(mean_res)
+print(var_res)
+print(std_res
