1b8407a2cb6448f9bb0478ff0b63268fcd257e4a
olafilova <ofilova@gmail.com>
2025-01-28 09:46:32 +0000

    numpy

--- hacker-27-1-25.py
+++ hacker-27-1-25.py
@@ -10,4 +10,11 @@ std_res = round(np.std(array), 11)
 
 print(mean_res)
 print(var_res)
-print(std_res
+print(std_res)
+
+
+A = np.array([1, 2, 3])  # First vector
+B = np.array([4, 5, 6])  # Second vector
+
+cross_product = np.cross(A, B)  # Calculate the cross product
+print(cross_product)
