da248d619b98e753aa4c51335c9708f2103be245
olafilova <ofilova@gmail.com>
2025-01-28 09:44:54 +0000

    numpy

--- hacker-27-1-25.py
+++ hacker-27-1-25.py
@@ -6,7 +6,7 @@ array = np.array([list(map(int, input().split())) for i in range(X)])
 
 mean_res = np.mean(array, axis=1)
 var_res = np.var(array, axis=0)
-std_res = round(np.std(array), 1)
+std_res = round(np.std(array), 11)
 
 print(mean_res)
 print(var_res)
